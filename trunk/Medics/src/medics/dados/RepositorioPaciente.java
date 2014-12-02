a7d661e01223cd1878826652e148bc8aeb523cc7
jnacmarcos@gmail.com <jnacmarcos@gmail.com@057c1a0b-2b81-eb48-7bc1-9c50485ab6b3>
2014-12-02 01:19:40 +0000

    Implementa a interface  IRepositorioPaciente em RepositorioPaciente

--- trunk/Medics/src/medics/dados/RepositorioPaciente.java
+++ trunk/Medics/src/medics/dados/RepositorioPaciente.java
@@ -1,12 +1,12 @@
 /**
  * Essa classe representa o repositorio de pacientes
  * 
- * As saÌdas e entradas s„o feitas na GUI e comunicacao 
- * È feita atravÈs da camada de dados, s„o passadas infomaÁıes
- * para camadas de dados que d· as repostas para a GUI. Para 
- * isso cÛdigos de comuniÁ„o s„o usados.
+ * As sa√≠das e entradas s√£o feitas na GUI e comunicacao 
+ * √© feita atrav√©s da camada de dados, s√£o passadas infoma√ß√µes
+ * para camadas de dados que d√° as repostas para a GUI. Para 
+ * isso c√≥digos de comuni√ß√£o s√£o usados.
  * 
- * Tabela de cÛdigos:
+ * Tabela de c√≥digos:
  * @codigo 1 - "Digite o nome do medico-"
  * @codigo 2 - "O que deseja editar ?"
  * @codigo 3 - "Novo nome- " 
@@ -15,12 +15,12 @@
  * @codigo 6 - "Novo bairro- "
  * @codigo 7 - "Novo telefone- "
  * @codigo 8 - "Novo email- "
- * @codigo 9 - "Paciente n„o encontrado."
- * @codigo 10 - "N„o existe pacientes."
+ * @codigo 9 - "Paciente n√£o encontrado."
+ * @codigo 10 - "N√£o existe pacientes."
  * @codigo 11 - "Lista vazia."
- * @codigo 12 - "Nome para remoÁ„o"
+ * @codigo 12 - "Nome para remo√ß√£o"
  * @codigo 13 - "Removido com sucesso."
- * @codigo 14 - "Paciente n„o encontrado !"
+ * @codigo 14 - "Paciente n√£o encontrado !"
  */
 
 package medics.dados;
@@ -28,137 +28,128 @@ package medics.dados;
 import medics.negocio.classes_basicas.Paciente;
 import medics.negocio.CadastroPaciente;
 
-public class RepositorioPaciente {
-	  private int contador = 0;
-	  Paciente[] lista;
-	  public static CadastroPaciente cadastroPaciente = new CadastroPaciente();
-	  	 
-	  public int getContador() {
+public class RepositorioPaciente implements IRepositorioPaciente {
+	private int contador = 0;
+	Paciente[] lista;
+	public static CadastroPaciente cadastroPaciente = new CadastroPaciente();
+
+	public int getContador() {
 		return contador;
-	  }
+	}
 
-	  public  void setContador(int contador) {
+	public void setContador(int contador) {
 		this.contador = contador;
-	  }
+	}
 
-	  public void criarLista() {
-	        lista = new Paciente[10];
-	  }
-	  
-      public Paciente[] getLista() {
+	public void criarLista() {
+		lista = new Paciente[10];
+	}
+
+	public Paciente[] getLista() {
 		return lista;
-	  }
+	}
 
-	  public void setLista(Paciente[] lista) {
+	public void setLista(Paciente[] lista) {
 		this.lista = lista;
-	  }
-	  
-	  public void aumentarArray(){
-	        Paciente[] auxiliar;
-	        auxiliar = new Paciente[lista.length];
-	        
-	        for(int i = 0 ; i<lista.length ; i++){
-	            auxiliar[i] = lista[i];
-	        }
-	        lista = new Paciente[auxiliar.length * 2];
-	        
-	        for(int i = 0; i<auxiliar.length ; i++){
-	            lista[i] = auxiliar[i];
-	        }
-	    }
-      
-      public void cadastrar(boolean primeiraVez, Paciente paciente) {
+	}
 
-          if(primeiraVez == true){
-              lista[0] = paciente;
-          }else{
-             contador++;
-             lista[contador] = paciente;
-          }
-      }
-      
-      public void modificar() {
-    	  if(lista != null){
-            boolean achou = false;
-            String nomeProcurado = cadastroPaciente.entradaParaRepositorioPaciente(1);
-            int indicePesquisa;
-         
-            for(indicePesquisa = 0 ; indicePesquisa <= contador && achou == false ; indicePesquisa++ ) {
-                String nomeDoArray = lista[indicePesquisa].getNome();
-                if (nomeProcurado.equals(nomeDoArray)){
-                    achou = true; 
-                }
-            }
-          
-              if (achou == true) {
-                String opt;
-                
-                opt = cadastroPaciente.entradaParaRepositorioPaciente(2);
-                switch (opt) {
-                    case "1":
-                        String novoNome = cadastroPaciente.entradaParaRepositorioPaciente(3);
-                        lista[indicePesquisa-1].setNome(novoNome);
-                        break;
-                    case "2":
-                        String novoCpf = cadastroPaciente.entradaParaRepositorioPaciente(4);
-                        lista[indicePesquisa-1].setCpf(novoCpf);
-                        break;
-                    case "3":
-                        String novoData = cadastroPaciente.entradaParaRepositorioPaciente(5);
-                        lista[indicePesquisa-1].setDataNascimento(novoData);;
-                        break;
-                    case "4":
-                        String novoBairro = cadastroPaciente.entradaParaRepositorioPaciente(6);
-                        lista[indicePesquisa-1].setBairro(novoBairro);
-                        break;
-                    case "5":
-                        String novoTelefone = cadastroPaciente.entradaParaRepositorioPaciente(7);
-                        lista[indicePesquisa-1].setTelefone(novoTelefone);
-                        break;
-                    case "6":
-                        String novoEmail = cadastroPaciente.entradaParaRepositorioPaciente(8);
-                        lista[indicePesquisa-1].setEmail(novoEmail);
-                        break;
-                }
-            } else {
-                cadastroPaciente.saidaParaRepositorioPaciente(9);
-            }
-    	  }else{
-    		  cadastroPaciente.saidaParaRepositorioPaciente(10);
-    	  }
-      }
-     
-      public void exibir(){
-    	  if(lista[0] != null){
-    	     cadastroPaciente.exibirListaProcedimentoPaciente(lista, contador); //GUI imprimi lista
-    	  }else{
-    		  cadastroPaciente.saidaParaRepositorioPaciente(11);
-    	  }
-      }
-      
-      public void remover(){
-         
-    	  if(lista != null){
-            String nome = cadastroPaciente.entradaParaRepositorioPaciente(12);
-            boolean achou = false;
-            int i;
-          
-            for(i = 0 ; i <= contador && achou == false ; i++){
-               String auxiliarNome = lista[i].getNome();
-              
-               if(nome.equals(auxiliarNome)){
-                   achou = true;
-               }
-            }
-          
-            if(achou == true){
-        	    cadastroPaciente.saidaParaRepositorioPaciente(13);
-                lista[i] = lista[contador];
-                lista[contador] = null;
-            }else{
-        	    cadastroPaciente.saidaParaRepositorioPaciente(14);
-            }
-    	  }
-      }
+	public void aumentarArray() {
+		Paciente[] auxiliar;
+		auxiliar = new Paciente[lista.length];
+
+		for (int i = 0; i < lista.length; i++) {
+			auxiliar[i] = lista[i];
+		}
+		lista = new Paciente[auxiliar.length * 2];
+
+		for (int i = 0; i < auxiliar.length; i++) {
+			lista[i] = auxiliar[i];
+		}
+	}
+
+	public void cadastrar(boolean primeiraVez, Paciente paciente) {
+
+		if (primeiraVez == true) {
+			lista[0] = paciente;
+		} else {
+			contador++;
+			lista[contador] = paciente;
+		}
+
+	}
+
+	public void modificar(String nome, String opt, String novo) {
+		if (lista != null && lista[0] != null) {
+			boolean achou = false;
+			int indicePesquisa;
+
+			for (indicePesquisa = 0; indicePesquisa <= contador
+					&& achou == false; indicePesquisa++) {
+				String nomeDoArray = lista[indicePesquisa].getNome();
+				if (nome.equals(nomeDoArray)) {
+					achou = true;
+				}
+			}
+
+			if (achou == true) {
+
+				switch (opt) {
+				case "1":
+					lista[indicePesquisa - 1].setNome(novo);
+					break;
+				case "2":
+					lista[indicePesquisa - 1].setCpf(novo);
+					break;
+				/*
+				 * case "3": try { DateFormat df = new
+				 * SimpleDateFormat("dd/MM/yyyy"); Date data =
+				 * df.parse(novoData); lista[indicePesquisa -
+				 * 1].setDataNascimento(data); } catch (Exception ex) {
+				 * ex.printStackTrace(); }
+				 * 
+				 * break;
+				 */
+				case "4":
+					lista[indicePesquisa - 1].setBairro(novo);
+					break;
+				case "5":
+					lista[indicePesquisa - 1].setTelefone(novo);
+					break;
+				case "6":
+					lista[indicePesquisa - 1].setEmail(novo);
+					break;
+				}
+			}
+		}
+	}
+
+	public void exibir() {
+		if (lista != null && lista[0] != null) {
+			//cadastroPaciente.exibirListaProcedimentoPaciente(lista, contador);
+		} else {
+			//cadastroPaciente.saidaParaRepositorioPaciente(11);
+		}
+	}
+
+	public void remover(String nome) {
+		boolean achou = false;
+		if (lista != null && lista[0] != null) {
+			int i;
+
+			for (i = 0; i <= contador && achou == false; i++) {
+				String auxiliarNome = lista[i].getNome();
+
+				if (nome.equals(auxiliarNome)) {
+					achou = true;
+				}
+			}
+
+			if (achou == true) {
+				lista[i] = lista[contador];
+				lista[contador] = null;
+			}
+
+		}
+
+	}
 }
-
