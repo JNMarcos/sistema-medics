19fd3238b02b243bd0892a0f82f1dd9ba12c9b5b
jnacmarcos@gmail.com <jnacmarcos@gmail.com@057c1a0b-2b81-eb48-7bc1-9c50485ab6b3>
2014-12-22 18:40:47 +0000

    Mudanças devido à classe Endereco.

--- Medics/src/medics/negocio/classes_basicas/Pessoa.java
+++ Medics/src/medics/negocio/classes_basicas/Pessoa.java
@@ -9,12 +9,8 @@ public abstract class Pessoa implements Comparable<Pessoa>, Serializable{
 	private String dia;
 	private String mes;
 	private String ano;
-	private String rua;
-	private String bairro;
-	private String cidade;
-	private String telefone;
-	private String email;
-
+  private Endereco endereco;
+ 
 	public String getPrimeiroNome() {
 		return primeiroNome;
 	}
@@ -62,47 +58,15 @@ public abstract class Pessoa implements Comparable<Pessoa>, Serializable{
 	public void setAno(String ano) {
 		this.ano = ano;
 	}
+   
+   public Endereco getEndereco() {
+     return endereco;
+   }
+   public void setEndereco(Endereco endereco) {
+     this.endereco = endereco;
+   }
 
-	public String getRua() {
-		return rua;
-	}
-
-	public void setRua(String rua) {
-		this.rua = rua;
-	}
-
-	public String getBairro() {
-		return bairro;
-	}
-
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
-
-	public String getCidade() {
-		return cidade;
-	}
-
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
