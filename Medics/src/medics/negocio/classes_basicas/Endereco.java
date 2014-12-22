08d608d058b2d30aad2e08ab1b08ef044bc9759a
jnacmarcos@gmail.com <jnacmarcos@gmail.com@057c1a0b-2b81-eb48-7bc1-9c50485ab6b3>
2014-12-22 18:34:45 +0000

    Cisão da classe Pessoa. Transformou-se parte na classe Endereco para manter a ideia de pessoa e separar os conceitos.

---
+++ Medics/src/medics/negocio/classes_basicas/Endereco.java
@@ -0,0 +1,59 @@
+public class Endereco {
+	private String rua;
+	private short numero;
+	private short complemento;
+	private String cep;
+	private String bairro;
+	private String cidade;
+	private String telefone;
+	private String email;
+	
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public short getNumero() {
+		return numero;
+	}
+	public void setNumero(short numero) {
+		this.numero = numero;
+	}
+	public short getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(short complemento) {
+		this.complemento = complemento;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}		
+}
